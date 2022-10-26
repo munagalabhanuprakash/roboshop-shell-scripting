5339799bcdc30a043a1f4d1c6b08e5f6e1c12c24
username <bhanuprakashmunagala@gmail.com>
2022-10-27 05:17:22 +0530

    Corrected code

--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -8,8 +8,8 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
-mv frontend-main/static/* .
+mv frontend-main/* .
 mv static/* .
-rm-rf frontend-main README.md
+rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
