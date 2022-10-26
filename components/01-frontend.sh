2c8afe63758767d5b3e92b6bf94ec664370cf2eb
username <bhanuprakashmunagala@gmail.com>
2022-10-26 12:04:33 +0530

    Added frontend script

--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo This is frontend script
+echo Running frontend script..
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf * unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
