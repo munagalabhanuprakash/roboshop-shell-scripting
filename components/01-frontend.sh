b38c7cb80017364ceb459c53573107cced230450
username <bhanuprakashmunagala@gmail.com>
2022-10-26 12:14:08 +0530

    Corrected code

--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -4,10 +4,12 @@ echo Running frontend script..
 
 yum install nginx -y
 systemctl enable nginx
-systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
-rm -rf * unzip /tmp/frontend.zip
+rm -rf *
+unzip /tmp/frontend.zip
 mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+mv static/* .
+rm-rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
