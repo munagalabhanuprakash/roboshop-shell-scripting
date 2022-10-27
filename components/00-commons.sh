5000515122b85155161f3f93e68ec0e8118cc8b2
username <bhanuprakashmunagala@gmail.com>
2022-10-27 06:54:30 +0530

    added colors to the echo

--- components/00-commons.sh
+++ components/00-commons.sh
@@ -5,7 +5,7 @@ CHECKROOTUSER()
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne "0" ];
 then
-  echo You are supposed to run these commands using sudo
+  echo -e  "\e[31mYou are supposed to run these commands using sudo\e[0m"
   exit
 else
   echo OK
