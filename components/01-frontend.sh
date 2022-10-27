fb93d31b4623468b798b8781a0dd27717a5721c3
username <bhanuprakashmunagala@gmail.com>
2022-10-27 09:54:04 +0530

    corrected code in commons

--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -3,14 +3,14 @@
 source components/00-commons.sh
 CHECKROOTUSER
 
-yum install nginx -y
-systemctl enable nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+yum install nginx -y > /tmp/frontendoutput
+systemctl enable nginx > /tmp/frontendoutput
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" > /tmp/frontendoutput
+cd /usr/share/nginx/html > /tmp/frontendoutput
+rm -rf * > /tmp/frontendoutput
+unzip /tmp/frontend.zip > /tmp/frontendoutput
+mv frontend-main/* . > /tmp/frontendoutput
+mv static/* . > /tmp/frontendoutput
+rm -rf frontend-main README.md > /tmp/frontendoutput
+mv localhost.conf /etc/nginx/default.d/roboshop.conf > /tmp/frontendoutput
+systemctl restart nginx > /tmp/frontendoutput
