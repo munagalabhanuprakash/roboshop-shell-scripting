3d67b959d350ce77d097c73072679b777c5d2b91
username <bhanuprakashmunagala@gmail.com>
2022-10-27 10:23:30 +0530

    Added echo  and redirecting the output to output file

--- components/00-commons.sh
+++ components/00-commons.sh
@@ -10,14 +10,12 @@ then
 fi
 }
 
-COMMANDSTATUSCHECK()
-{
-if [ $? -eq 0 ]; then
-  echo -e “\e[32mSUCCESS\e[0m”
-else
-  echo -e “\e[31mFAILURE\e[0m”
-  exit 1
-  fi
-}
-
-COMMANDSTATUSCHECK
+# COMMANDSTATUSCHECK()
+#{
+#if [ $? -eq 0 ]; then
+#  echo -e "\e[32mSUCCESS\e[0m"
+#else
+#  echo -e "\e[31mFAILURE\e[0m"
+#  exit 1
+#  fi
+#}
--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -2,30 +2,48 @@
 
 source components/00-commons.sh
 CHECKROOTUSER
+
 echo "Installing NGINX.."
 yum install nginx -y > /tmp/frontendoutput
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 1
+  fi
 
 echo "Enabling NGINX.."
 systemctl enable nginx > /tmp/frontendoutput
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mFAILURE\e[0m"
+  exit 1
+  fi
 
 echo "Downloading frontend.zip.."
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" > /tmp/frontendoutput
+
 
 cd /usr/share/nginx/html > /tmp/frontendoutput
 
 echo "removing old files.."
 rm -rf * > /tmp/frontendoutput
 
+
 echo "unzipping frontend.zip.."
 unzip /tmp/frontend.zip > /tmp/frontendoutput
+
 
 echo "copying extracted content.."
 mv frontend-main/* . > /tmp/frontendoutput
 mv static/* . > /tmp/frontendoutput
 rm -rf frontend-main README.md > /tmp/frontendoutput
 
+
 echo "copying nginx roboshop config.."
 mv localhost.conf /etc/nginx/default.d/roboshop.conf > /tmp/frontendoutput
+
 
 echo "restarting nginx.."
 systemctl restart nginx > /tmp/frontendoutput
