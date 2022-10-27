e829c21b3a7981e28851808b1bd4c00d2e8c1100
username <bhanuprakashmunagala@gmail.com>
2022-10-27 10:37:41 +0530

    Added echo  and redirecting the output to output file

--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -5,36 +5,36 @@ checkRootUser
 
 echo "Installing NGINX.."
 yum install nginx -y > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 echo "Enabling NGINX.."
 systemctl enable nginx > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 echo "Downloading frontend.zip.."
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 cd /usr/share/nginx/html > /tmp/frontendoutput
 
 echo "removing old files.."
 rm -rf * > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 echo "unzipping frontend.zip.."
 unzip /tmp/frontend.zip > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 echo "copying extracted content.."
 mv frontend-main/* . > /tmp/frontendoutput
 mv static/* . > /tmp/frontendoutput
 rm -rf frontend-main README.md > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 echo "copying nginx roboshop config.."
 mv localhost.conf /etc/nginx/default.d/roboshop.conf > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
 
 echo "restarting nginx.."
 systemctl restart nginx > /tmp/frontendoutput
-StatusCheck $? $?
+StatusCheck $?
