433c14eef6e79b76641200555999b6d85297fd14
username <bhanuprakashmunagala@gmail.com>
2022-10-28 15:01:52 +0530

    Done changes in main program as per the new functions and variables added in commons.sh

--- components/01-frontend.sh
+++ components/01-frontend.sh
@@ -4,37 +4,37 @@ source components/00-commons.sh
 CHECKUSER
 
 ECHO "Installing NGINX.."
-yum install nginx -y >> ${LOG_FILE}
+yum install nginx -y >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
 
 ECHO "Enabling NGINX.."
-systemctl enable nginx >> ${LOG_FILE}
+systemctl enable nginx >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
 
 ECHO "Downloading frontend.zip.."
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >> ${LOG_FILE}
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
 
-cd /usr/share/nginx/html >> ${LOG_FILE}
+cd /usr/share/nginx/html >>${LOG_FILE}
 
 ECHO "removing old files.."
-rm -rf * >> ${LOG_FILE} 
+rm -rf * >>${LOG_FILE} 
 COMMANDSTATUSCHEK $?
 
 ECHO "unzipping frontend.zip.."
-unzip /tmp/frontend.zip >> ${LOG_FILE}
+unzip /tmp/frontend.zip >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
 
 ECHO "copying extracted content.."
-mv frontend-main/* . >> ${LOG_FILE}
-mv static/* . >> ${LOG_FILE}
-rm -rf frontend-main README.md >> ${LOG_FILE}
+mv frontend-main/* . >>${LOG_FILE}
+mv static/* . >>${LOG_FILE}
+rm -rf frontend-main README.md >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
 
 ECHO "copying nginx roboshop config.."
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >> ${LOG_FILE}
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
 
 ECHO "restarting nginx.."
-systemctl restart nginx >> ${LOG_FILE}
+systemctl restart nginx >>${LOG_FILE}
 COMMANDSTATUSCHEK $?
